ccf91304a14b10c3f15572e2d316c9b8812e4e89
SMani24 <mmanimirshabani24@gmail.com>
2026-07-17 20:24:04 +0330

    Add Bullet

---
+++ include/Bullet.hpp
@@ -0,0 +1,35 @@
+/* ========== Naming Convention Guideline ==========
+ * Class names: PascalCase
+ * Function names : camelCase
+ * Variable names : camelCase
+ * Constant names : UPPER_SNAKE_CASE
+ * ================================================= */
+
+#ifndef BULLET_HPP
+#define BULLET_HPP
+
+#include <SFML/Graphics.hpp>
+#include <memory>
+
+namespace BulletConfig {
+    constexpr float SPEED = 800.0f;
+}
+
+class Bullet {
+private:
+    sf::Sprite sprite;
+    float posX;
+    float posY;
+
+public:
+    Bullet(std::shared_ptr<sf::Texture> tex, float startX, float startY);
+    ~Bullet() = default;
+
+    void update(sf::Time deltaTime);
+    void draw(sf::RenderWindow& window) const;
+    
+    float getY() const;
+    sf::FloatRect getBounds() const;
+};
+
+#endif // BULLET_HPP
---
+++ src/Bullet.cpp
@@ -0,0 +1,33 @@
+/* ========== Naming Convention Guideline ==========
+ * Class names: PascalCase
+ * Function names : camelCase
+ * Variable names : camelCase
+ * Constant names : UPPER_SNAKE_CASE
+ * ================================================= */
+
+#include "Bullet.hpp"
+
+Bullet::Bullet(std::shared_ptr<sf::Texture> tex, float startX, float startY) 
+    : posX(startX), posY(startY) 
+{
+    sprite.setTexture(*tex);
+    sprite.setOrigin(sprite.getLocalBounds().width / 2.0f, sprite.getLocalBounds().height / 2.0f);
+    sprite.setPosition(posX, posY);
+}
+
+void Bullet::update(sf::Time deltaTime) {
+    posY -= BulletConfig::SPEED * deltaTime.asSeconds();
+    sprite.setPosition(posX, posY);
+}
+
+void Bullet::draw(sf::RenderWindow& window) const {
+    window.draw(sprite);
+}
+
+float Bullet::getY() const {
+    return posY;
+}
+
+sf::FloatRect Bullet::getBounds() const {
+    return sprite.getGlobalBounds();
+}
